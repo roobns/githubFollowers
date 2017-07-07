f90db18e10284c07763142e829f9e49d8e19945b
Ruben <rlazo@actinver.com.mx>
2017-07-07 14:55:08 -0500

    Dashboard / Get git users and limit to 10

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var app = angular.module('followersApp', ['ui.router'])
+var app = angular.module('followersApp', ['ui.router', 'ui.bootstrap'])
 
 app.config(function($stateProvider, $urlRouterProvider) {
     
@@ -13,12 +13,38 @@ app.config(function($stateProvider, $urlRouterProvider) {
             templateUrl: '../views/main.html',
             controller: 'mainCtrl'
         })
+        .state('about', {
+            url: '/',
+            templateUrl: '../views/about.html',
+            controller: 'aboutCtrl'
+        })
         
         
 });
 
-app.controller('mainCtrl', function($scope) {
+app.controller('mainCtrl', function($http, $scope) {
+
+
+    function init(){
+        getUsers();
+        $scope.showModal = false;
+    };
     
-   console.log("aaaa");
-    
+    function getUsers(){
+        $http({method: 'GET', url: 'https://api.github.com/users'}).
+        then(function(response) {
+            $scope.users = response.data;
+            console.log($scope.users);
+        }, function(response) {
+            $scope.users = response.data || 'Request failed';
+        });
+    }
+
+    init();
+});
+
+app.component("gitHubFollowers",{
+      templateUrl: "../views/gitFollowers.html",
+      bindings: { name: '@' },
+      controller: 'mainCtrl'
 });
